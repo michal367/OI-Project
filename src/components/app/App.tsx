8867c048f2ba46f068b523be38de167ca9e2e76e
Ostatni5 <>
2021-03-22 17:59:21 +0100

    move & rename create-session-view

---
+++ src/components/app/App.tsx
@@ -0,0 +1,38 @@
+import { CssBaseline } from "@material-ui/core";
+import { createMuiTheme, ThemeProvider } from "@material-ui/core/styles";
+import { CreateSessionView } from "../createSessionView/CreateSessionView";
+import "fontsource-roboto";
+
+const theme = createMuiTheme({
+    palette: {
+        primary: {
+            // light: will be calculated,
+            light: "#80A3E4",
+            main: "#4870AC",
+            // dark: "#404756",
+            // contrastText: will be calculated,
+        },
+        secondary: {
+            light: "#FFEECB",
+            main: "#D9A21B",
+        },
+        // Used by `getContrastText()` to maximize the contrast between
+        // the background and the text.
+        contrastThreshold: 3,
+        // Used by the functions below to shift a color's luminance by approximately
+        // two indexes within its tonal palette.
+        // E.g., shift from Red 500 to Red 300 or Red 700.
+        tonalOffset: 0.2,
+    },
+});
+
+function App() {
+    return (
+        <ThemeProvider theme={theme}>
+            <CssBaseline />
+            <CreateSessionView />
+        </ThemeProvider>
+    );
+}
+
+export default App;
---
+++ src/components/createSessionView/CreateSessionView.tsx
@@ -0,0 +1,99 @@
+import { useState, useRef, useEffect } from "react";
+import { Fab, CircularProgress } from "@material-ui/core";
+import { makeStyles, useTheme } from "@material-ui/core/styles";
+import { green } from "@material-ui/core/colors";
+import PlayArrowIcon from "@material-ui/icons/PlayArrow";
+import CheckIcon from "@material-ui/icons/Check";
+import clsx from "clsx";
+import "fontsource-roboto";
+
+export function CreateSessionView() {
+    const theme = useTheme();
+
+    const classes = makeStyles({
+        root: {
+            background: theme.palette.secondary.light,
+            gap: "50px",
+            minHeight: "100vh",
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+        },
+        wrapper: {
+            position: "relative",
+        },
+        buttonSuccess: {
+            backgroundColor: green[500],
+            "&:hover": {
+                backgroundColor: green[700],
+            },
+        },
+        fabProgress: {
+            color: green[500],
+            position: "absolute",
+            top: -6,
+            left: -6,
+            zIndex: 1,
+        },
+        sessionBtn: {
+            width: "200px",
+            height: "200px",
+            fontSize: "150px",
+        },
+        header: {
+            fontSize: "90px",
+            color: theme.palette.primary.dark,
+        },
+    })();
+
+    const [loading, setLoading] = useState(false);
+    const [success, setSuccess] = useState(false);
+    const timer = useRef<number>();
+    const buttonClassname = clsx({
+        [classes.sessionBtn]: 1,
+        [classes.buttonSuccess]: success,
+    });
+
+    useEffect(() => {
+        return () => {
+            clearTimeout(timer.current);
+        };
+    }, []);
+
+    const handleButtonClick = () => {
+        if (!loading) {
+            setSuccess(false);
+            setLoading(true);
+            timer.current = window.setTimeout(() => {
+                setSuccess(true);
+                setLoading(false);
+            }, 2000);
+        }
+    };
+
+    return (
+        <div className={classes.root}>
+            <h1 className={classes.header}>Stwórz sesję</h1>
+            <div className={classes.wrapper}>
+                <Fab
+                    aria-label="save"
+                    color="primary"
+                    className={buttonClassname}
+                    onClick={handleButtonClick}
+                >
+                    {success ? (
+                        <CheckIcon fontSize="inherit" />
+                    ) : (
+                        <PlayArrowIcon fontSize="inherit" />
+                    )}
+                </Fab>
+                {loading && (
+                    <CircularProgress
+                        size={210}
+                        className={classes.fabProgress}
+                    />
+                )}
+            </div>
+        </div>
+    );
+}
