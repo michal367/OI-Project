aac35dd020378789cb4a704634f9953216a0db26
michal367 <michal.krawczyk7@gmail.com>
2021-06-09 08:11:10 +0200

    limit open questions answer (#343)

--- src/lecturer/components/app/App.tsx
+++ src/lecturer/components/app/App.tsx
@@ -39,6 +39,7 @@ function App() {
     const store = useContext(StoreContext);
     const { socketEmiter, sendJsonMessage } = useSocket(); //for keeping socket open
     const [isLectureStarted, setIsLectureStarted] = useState<boolean>(!!store.lectureID);
+    
     // heroku 55s timeout fix
     useEffect(() => {
         if (window.location.hostname.includes("heroku")) {
