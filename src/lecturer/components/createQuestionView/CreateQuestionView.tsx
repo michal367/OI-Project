27c9d634cb7250f83bbad0a5e5c5807c88456808
ostatni5 <26521377+ostatni5@users.noreply.github.com>
2021-06-09 09:00:11 +0200

    fix question adding

--- src/lecturer/components/createQuestionView/CreateQuestionView.tsx
+++ src/lecturer/components/createQuestionView/CreateQuestionView.tsx
@@ -335,7 +335,8 @@ export function CreateQuestionView() {
     }, [QuestionType, answers, mode, noError, question, store.questions, title]);
 
     const timer = useRef<number>();
-    const handleSubmit = useCallback(() => {
+    const handleSubmit = useCallback((event) => {
+        event.preventDefault();
         if (!loading) {
 
             if (!validate()) {
@@ -396,7 +397,7 @@ export function CreateQuestionView() {
           if (event.code === "Enter" || event.code === "NumpadEnter") {
             event.preventDefault();
             if (!loading){
-                handleSubmit();
+                handleSubmit(event);
             } 
           }
         };
